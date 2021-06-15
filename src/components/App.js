daf12a755d7bec6526504c0739b44a24fe65b6c8
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-06-15 11:11:37 +0200

    wip

--- src/components/App.js
+++ src/components/App.js
@@ -7,11 +7,28 @@ const StyledApp = styled.div`
 `
 
 export default function App() {
-  const [message, setMessage] = useState('')
+  const [questions, setQuestions] = useState([])
 
-  useEffect(async() => {
-    const res = await axios.get('http://localhost:5000/api/questions/7')
-    setMessage(res.data)
+  // useEffect(async () => {
+  //   const res = await axios.get('http://localhost:5000/api/questions/7')
+  //   setQuestions(questions.concat(res.data))
+  // }, [])
+
+  // useEffect(async () => {
+  //   const res = await fetch('http://localhost:5000/api/questions/2')
+  //   const data = await res.json()
+  //   setQuestions(questions.concat(data))
+  // }, [])
+
+  // useEffect(() => {
+  //   axios.get('http://localhost:5000/api/questions/7')
+  //     .then(res => setQuestions(questions.concat(res.data)))
+  // }, [])
+
+  useEffect(() => {
+    fetch('http://localhost:5000/api/questions/2')
+      .then(res => res.json())
+      .then(data => setQuestions(questions.concat(data)))
   }, [])
 
   return (
@@ -19,8 +36,8 @@ export default function App() {
       <h1>lady gaga</h1>
       <h3>investigating Codegrade</h3>
       This is the flow students are used to
-      {/* <div>{JSON.stringify(message)}</div> */}
-      <h4>{message.question_title}</h4>
+      {/* <div>{JSON.stringify(questions)}</div> */}
+      <h4>{!!questions.length && questions[0].question_title}</h4>
     </StyledApp>
   )
 }
